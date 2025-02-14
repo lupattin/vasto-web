a541ef4bd9f97113df6ee2b6fc0e8e430a32be7c
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-14 19:29:27 -0300

    fragments in page

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Ours from "@/components/ours/ours";
 import ContactForm from "@/components/contactform/contactform";
 export default function Home() {
   return (
-    <div >
+    <>
       <Initial />
       <Welcomeinitial/>
       <Thesolution/>
