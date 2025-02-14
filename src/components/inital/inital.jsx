ef42cdd64fb09baae6d8965393d916c3fe23db7d
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-14 20:17:35 -0300

    fixing width errors

--- src/components/inital/inital.jsx
+++ src/components/inital/inital.jsx
@@ -7,7 +7,7 @@ export default function Initial() {
             <h1 className={styles.titletag}>Empowering<br/> regional <br/>supermarkets</h1>
             <p className={styles.paragraphtag}>Vasto empowers regional supermarkets with better pricing, quality, and efficiency through <br/> technology-driven supply chain optimization</p>
         </div>
-        <Image src={"/img/backvastofoodfield.png"} alt='imagenCampoSembrado' fill style={{zIndex: '-1'} }></Image>
+        <Image className={styles.image} src={"/img/backvastofoodfield.png"} alt='imagenCampoSembrado' fill style={{zIndex: '-1'} }></Image>
       </div>
       </>
     );
