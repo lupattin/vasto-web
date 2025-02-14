020ec05e79de9995fe650f82d5b247229f405409
Luca Pattin <lupattin@gmail.com>
2025-02-14 20:39:08 -0300

    add logic to get in touch in /about

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,7 +2,24 @@
 import styles from './styles.module.css';
 import Link from "next/link";
 import Image from 'next/image';
+
+import { useRouter } from "next/navigation";
 export default function Navbar() {
+
+  const router = useRouter();
+
+  const handleGetInTouch = () => {
+    if (window.location.pathname === "/about") {
+      // Redirige a la página principal y espera que cargue antes de hacer scroll
+      router.push("/");
+      setTimeout(() => {
+        document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" });
+      }, 2000); // Espera un poco para asegurar que la página cargue
+    } else {
+      // Si ya está en la página principal, solo hace scroll
+      document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" });
+    }
+  };
     return (
 
       <div className={styles.container} >
@@ -12,8 +29,12 @@ export default function Navbar() {
         <div className={styles.containerLinks}>
           <Link href={'./'}>Home</Link>
           <Link href={'./about'}>About Us</Link>
-          <button className={styles.containerButton} onClick={() => document.getElementById("contact")?.scrollIntoView({ behavior: "smooth" })}>Get in touch</button>
+          <button className={styles.containerButton} onClick={handleGetInTouch}>
+          Get in touch
+        </button>
         </div>
       </div>
     ); 
   }
+
+  /*  */
