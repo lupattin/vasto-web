e94079bf90911b5da61d56c1e2381bb4027498fb
florenciaParodi <florenciaparodi1798@gmail.com>
2025-02-14 21:09:34 -0300

    some responsive changes - styled details

--- src/UI/cards/cards.jsx
+++ src/UI/cards/cards.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export function CardsTeam({ title, subtitle, item1, item2, item3, image }) {
   return (
     <div className={styles.container}>
-        <Image src={image} width={250} height={250} alt='foto de miembro del equipo' className={styles.cardImage}></Image>
+        <Image className={styles.image} src={image} width={250} height={250} alt='foto de miembro del equipo' className={styles.cardImage}></Image>
         <h3 className={styles.cardTitle}> {title} </h3>
         <h4 className={styles.cardSubtitle}> {subtitle}</h4>
         <p className={styles.cardItem}>{item1}</p>
