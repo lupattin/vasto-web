6a33e708a5ca11cf683e50fccd459fa553aa24d5
Luca Pattin <lupattin@gmail.com>
2025-03-27 18:31:23 -0300

    Test new animation

--- src/components/ours/ours.jsx
+++ src/components/ours/ours.jsx
@@ -15,12 +15,12 @@ useEffect(() => {
 
 return (
     <div className={styles.container} >
-        <div className={styles.textContainer} data-aos="fade-left">
+        <div className={styles.textContainer} data-aos="fade-up">
             <h4 className={styles.titleLeft}> OUR VISION </h4>
             <div></div>
             <p className={styles.pharagraphLeft}> To modernize the fresh produce industry with cutting-edge technlogy </p>
         </div>
-        <div className={styles.textContainer} data-aos="fade-right">
+        <div className={styles.textContainer} data-aos="fade-up">
             <h4 className={styles.titleRight}> OUR MISSION </h4>
             <div></div>
             <p className={styles.pharagraphRight}> Become the broadest and  most reliable supplier for  regional supermarkets,  spear-heading their  technological evolution. </p>
